5d0ca4a95a7ae14f61d5533d6f4fb93b3cce82d7
Alicio Valle <71409214+ValleA242@users.noreply.github.com>
2022-12-08 01:28:14 -0500

    Update index.js
    - switched if else statement to switch statement 
    - removed global variable 
    - added innerhtml variable within for loop
    - added function to event listener

--- index.js
+++ index.js
@@ -1,61 +1,47 @@
-function clickEvent(){
-    let drumArray = document.querySelectorAll(".drum") 
-
-    function kickDrumTrigger(){
-        let kickDrum = new Audio('/sounds/kick-bass.mp3')
-        kickDrum.play();
-    }
-
-    function snareDrumTrigger(){
-        let snareDrum = new Audio('/sounds/snare.mp3')
-        snareDrum.play();
-    }
-
-    function crashTrigger(){
-        let crash = new Audio('/sounds/crash.mp3')
-        crash.play();
-    }
-
-    function tomOneTrigger(){
-        let tom1 = new Audio('/sounds/tom-1.mp3')
-        tom1.play();
-    }
-
-    function tomTwoTrigger(){
-        let tom2 = new Audio('/sounds/tom-2.mp3')
-        tom2.play();
-    }
-
-    function tomThreeTrigger(){
-        let tom3 = new Audio('/sounds/tom-3.mp3')
-        tom3.play();
-    }
-
-    function tomFourTrigger(){
-        let tom4 = new Audio('/sounds/tom-4.mp3')
-        tom4.play();
-    }
-
-
- for(i = 0; i < drumArray.length; i++){
-    if(drumArray [i] === drumArray[0]){    
-            drumArray[0].addEventListener("click", kickDrumTrigger);
-        } else if(drumArray[i] === drumArray[1]){
-            drumArray[1].addEventListener("click", snareDrumTrigger);
-        } else if(drumArray[i] === drumArray[2]){
-            drumArray[2].addEventListener("click", crashTrigger);
-        } else if(drumArray[i] === drumArray [3]){
-            drumArray[3].addEventListener("click", tomOneTrigger);
-        } else if(drumArray[i] === drumArray[4]){
-            drumArray[4].addEventListener("click", tomTwoTrigger);
-        } else if(drumArray[i] === drumArray[5]){
-            drumArray[5].addEventListener("click", tomThreeTrigger);
-        } else {
-            drumArray[6].addEventListener("click", tomFourTrigger);
-        }
+for(i = 0; i < drumArray.length; i++){
     
+   document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+     
+    let buttonInnerHTML = this.innerHTML;
+     
+    switch (buttonInnerHTML) {
+        case "w":
+           let kickDrum = new Audio('/sounds/kick-bass.mp3');
+           kickDrum.play(); 
+           break;
+         
+        case "a":
+            let snareDrum = new Audio('/sounds/snare.mp3');
+            snareDrum.play();
+            break;
+             
+        case "s":
+            let crash = new Audio('/sounds/crash.mp3');
+            crash.play();
+            break;
+         
+        case "d":
+            let tomOne = new Audio('/sounds/tom-1.mp3');
+            tomOne.play();
+            break;
+          
+        case "j":
+            let tomTwo = new Audio('/sounds/tom-2.mp3');
+            tomTwo.play();
+            break;
+             
+        case "k":
+            let tomThree = new Audio('/sounds/tom-3.mp3');
+            tomThree.play();
+            break;
+             
+        case "l":
+            let tomFour = new Audio('/sounds/tom-4.mp3'); 
+            tomFour.play();
+            break;
+     }          
+     })
     }
 
-}
 
-clickEvent();
+
